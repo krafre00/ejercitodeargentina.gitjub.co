5598bda80317ea48d938cd3b66358379b27370ee
krafre00 <davidgongora412@gmail.com>
2025-04-23 14:39:58 -0500

    Delete scripts2.js

--- scripts2.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const userNameElement = document.getElementById('user-name');
-    if (userNameElement) {
-        const username = localStorage.getItem('username');
-        userNameElement.textContent = username ? username : 'Usuario';
-    }
-});
